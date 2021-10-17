12b3336b9f06cecc4c444f9645786f4999050da3
Edwin <edwin.r.warlock@gmail.com>
2021-10-16 19:41:24 -0500

    get_favorites function in favorites_db

--- database/favorites_db.py
+++ database/favorites_db.py
@@ -8,7 +8,7 @@ class Favorites(Model):
     country = CharField(null=False)
     month = IntegerField(null=False, constraints=[Check('0 < month AND month < 13')])
     year = IntegerField(null=False)
-    webcam = CharField()
+    webcam = CharField() # webcam URL(s)
     weather = CharField()
     holidays = CharField()
     nickname = CharField() # in case we want to allow the user to give their favorite a nickname
@@ -28,3 +28,17 @@ class FavoritesError(Exception):
 def create_table():
     """Create Favorites table."""
     db.create_tables([Favorites])
+
+
+def get_favorites():
+    """Select all favorites from the favorites table.
+    Return a list of favorites, or an empty list."""
+    # TODO: handle an empty list elsewhere in the program
+    # since we don't want there to be an error if the user has simply not added anything to faves yet.
+    # want html to instead display something like 'nothing in favorites list, try adding something to favorites'
+    favorites = Favorites.select()
+    favorites_list = []
+    for favorite in favorites:
+        favorites_list.append(favorite)
+
+    return favorites_list
