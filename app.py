e736c32c02c12e646be4f995e0165486e1e55713
Edwin <edwin.r.warlock@gmail.com>
2021-10-16 23:28:41 -0500

    index page with basic form for user

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, request, render_template, redirect
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home_page():
-    return "<h1>Travel Catalogue Program</h1>"
+    return render_template('index.html')
+
+
+# so far what we get from index is:
+# result?city=user_input&country=user_input&date=2021-12-23
