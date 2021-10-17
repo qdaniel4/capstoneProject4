8e88118420a4a96e18d6371ada1d9fb42e31d484
Edwin <edwin.r.warlock@gmail.com>
2021-10-16 19:34:06 -0500

    fix returned string in favorite model, create favorites.db

--- database/favorites_db.py
+++ database/favorites_db.py
@@ -4,7 +4,6 @@ db = SqliteDatabase('favorites.db')
 
 class Favorites(Model):
 
-    nickname = CharField() # in case we want to allow the user to give their favorite a nickname
     city = CharField(null=False)
     country = CharField(null=False)
     month = IntegerField(null=False, constraints=[Check('0 < month AND month < 13')])
@@ -12,18 +11,17 @@ class Favorites(Model):
     webcam = CharField()
     weather = CharField()
     holidays = CharField()
+    nickname = CharField() # in case we want to allow the user to give their favorite a nickname
 
     class Meta: 
         database = db
 
     def __str__(self):
-        question_info = f'ID. {self.id} Difficulty: {self.difficulty}, Points: {self.points}, Category: {self.category}'
-        question_and_answers = f'Q: {self.question}, A: {self.answercorrect}; {self.answerincorrecta}; {self.answerincorrectb}; {self.answerincorrectc}'
-        return f'{question_info}\n{question_and_answers}\n'
+        return f'{self.city}, {self.country}, in {self.month}/{self.year}: Webcam: {self.webcam}, Weather: {self.weather}, Holidays: {self.holidays}, Nickname: {self.nickname}'
 
 
 class FavoritesError(Exception):
-    """Custom exception class."""
+    """Custom exception class.""" 
     pass
 
 
