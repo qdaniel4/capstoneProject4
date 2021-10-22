e64e4aff57304316057850c8c198005dc8d04d03
Edwin <edwin.r.warlock@gmail.com>
2021-10-21 19:03:20 -0500

    change test_get_list_of_favorites_from_db to check that an empty list was not returned

--- test_favorites_db.py
+++ test_favorites_db.py
@@ -21,7 +21,7 @@ class FavoritesTest(TestCase):
     """
 
     def setUp(self):
-        '''Clear and remake favorites table for test database.'''
+        """Clear and remake favorites table for test database."""
         self.db = SqliteDatabase(test_db_path)
         self.db.drop_tables([Favorite])
         self.db.create_tables([Favorite])
@@ -33,13 +33,10 @@ class FavoritesTest(TestCase):
         favorite_one.save()
         favorite_two.save()
 
-        expected = []
-        expected.append('<Favorites: ID 1: City1, Country1, in 1/2020: Webcam: http://url.com/, Weather: weather, Holidays: holiday1, holiday2, Nickname: nickname>')
-        expected.append('<Favorites: ID 2: City2, Country2, in 7/2020: Webcam: http://url3.com/, Weather: weather, Holidays: holiday1, Nickname: nickname>')
+        empty_list = []
+        list_of_favorites = favorites_db.get_favorites()
 
-        result = favorites_db.get_favorites()
-
-        self.assertNotEqual(result, expected)
+        self.assertNotEqual(empty_list, list_of_favorites)
 
 
     def test_get_empty_list_from_db_when_no_records_in_db(self):
