f9561c583f82989f791d966e48d0f4c981415087
saida93522 <54591143+saida93522@users.noreply.github.com>
2021-10-22 17:03:36 -0500

    removed .ds_store file

--- holiday_cal/tests/test_main.py
+++ holiday_cal/tests/test_main.py
@@ -1,6 +1,5 @@
 from unittest import TestCase
 from unittest.mock import patch
-from unittest.mock import MagicMock
 
 class TestMain(TestCase):
     pass
--- holiday_cal/tests/test_ui.py
+++ holiday_cal/tests/test_ui.py
@@ -1,7 +1,5 @@
 from unittest import TestCase
 from unittest.mock import patch
-from unittest.mock import MagicMock
-
 
 class TestUI(TestCase):
     pass
