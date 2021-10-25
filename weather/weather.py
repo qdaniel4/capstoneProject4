f708b9aa7cd84ecd5afecfaa9af5cecc522973a6
frostbite169 <44807903+frostbite169@users.noreply.github.com>
2021-10-25 12:38:50 -0500

    comments some stuff out

--- weather/weather.py
+++ weather/weather.py
@@ -6,31 +6,22 @@ from pprint import pprint
 key = os.environ.get('TROPOSPHERE_KEY')
 
 
-def main():
-    city_input = city_name()
-    capitolized = capitolize_city(city_input)
-    list_of_countries = check_if_found(capitolized)
-    correct_city = pick_correct(list_of_countries)
-    latitude, longitude = get_coordinates(correct_city)
-    month = get_month_name()
-    month_to_check = get_month_number(month)
-    get_climate(latitude, longitude, month_to_check)
 
     
 
-def city_name():
-    not_match = True
-    while not_match:
-        city = input('enter a city ')
-        if city == '':
-            print('you must enter a city')
-        else:
-        # from automate the boring stuff book
-        # and from # from https://www.guru99.com/python-regular-expressions-complete-tutorial.html
-            check = re.match(r'[\d\W]', city)
-            if not check:
-                not_match = False
-                return city
+# def city_name():
+#     not_match = True
+#     while not_match:
+#         city = input('enter a city ')
+#         if city == '':
+#             print('you must enter a city')
+#         else:
+#         # from automate the boring stuff book
+#         # and from # from https://www.guru99.com/python-regular-expressions-complete-tutorial.html
+#             check = re.match(r'[\d\W]', city)
+#             if not check:
+#                 not_match = False
+#                 return city
 
 def capitolize_city(city):
     city = city.lower()
@@ -54,78 +45,90 @@ def check_if_found(searched_city):
     countries_list = []
     url_data = requests.get(url).json()
     if len(url_data['data']) == 0:
-        check_if_found(capitolize_city(city_name()))
-    for x in url_data['data']:
-        if searched_city == x['name']:
-            countries_list.append(x)
-    return countries_list  
+       return None
+    else:
+        for x in url_data['data']:
+            if searched_city == x['name']:
+                countries_list.append(x)
+        
+        return countries_list  
 
-def pick_correct(countries_list):
-    not_match = True
-    x = 0
-    while not_match:
-        x = 0
-        for country_in_list in countries_list:
-            print( str(x) + ' ' + country_in_list['name'] + ' in ' + country_in_list['admin1'] +', ' + country_in_list['country'])
-            x += 1
-        num = input('enter the number of correct city ')
-        if  not num.isnumeric():
-            print('you must enter a number')
-        elif num == '':
-            print('you must enter a number')
+def pick_correct(countries_list, country):
+    cities_in_country = []
+    for correct_country in countries_list:
+        if country == correct_country['country']:
+            cities_in_country.append(correct_country)
+        if len(cities_in_country) == 0:
+            return None
         else:
-            num = int(num)
-            if num > int(len(countries_list)) :
-                print('you must enter a lower number')
-            elif  num < 0:
-                print('you must enter a higher number')
-            else:
-                correct_city = countries_list[num]
-                not_match = False
-                return correct_city
+            print(cities_in_country[0])
+            return cities_in_country[0]
+       # if country == correct_country[]
+    # not_match = True
+    # x = 0
+    # while not_match:
+    #     x = 0
+    #     for country_in_list in countries_list:
+    #         print( str(x) + ' ' + country_in_list['name'] + ' in ' + country_in_list['admin1'] +', ' + country_in_list['country'])
+    #         x += 1
+    #     num = input('enter the number of correct city ')
+    #     if  not num.isnumeric():
+    #         print('you must enter a number')
+    #     elif num == '':
+    #         print('you must enter a number')
+    #     else:
+    #         num = int(num)
+    #         if num > int(len(countries_list)) :
+    #             print('you must enter a lower number')
+    #         elif  num < 0:
+    #             print('you must enter a higher number')
+    #         else:
+    #             correct_city = countries_list[num]
+    #             not_match = False
+    #             return correct_city
     
 def get_coordinates(correct_city):
     latitude = correct_city['latitude']
     longitude = correct_city['longitude']
     return latitude, longitude
     
-def get_month_name():
-    not_match = True
-    while not_match:
-        month = input('enter a month name ')
-        if month == '':
-            print('you must enter a month name')
-        else:
-            check = re.match(r'[\d\W]', month)
-            if not check:
-                month = month.lower()
-                if month == 'january' or month == 'febuary' or month == 'march' or month == 'april' or month == 'may' or month == 'june' or month == 'july' or month == 'august' or month == 'september' or month == 'october' or month == 'november' or month == 'december':
-                    not_match = False
-                    return month
+# def get_month_name():
+#     not_match = True
+#     while not_match:
+#         month = input('enter a month name ')
+#         if month == '':
+#             print('you must enter a month name')
+#         else:
+#             check = re.match(r'[\d\W]', month)
+#             if not check:
+#                 month = month.lower()
+#                 if month == 'january' or month == 'febuary' or month == 'march' or month == 'april' or month == 'may' or month == 'june' or month == 'july' or month == 'august' or month == 'september' or month == 'october' or month == 'november' or month == 'december':
+#                     not_match = False
+#                     return month
 
 def get_month_number(month):
     number = 0
-    if month == 'january':
+    if month == 1:
         number = 0
-    elif month == 'febuary':
+    elif month == 2:
         number = 1
-    elif month == 'march':
+    elif month == 3:
         number = 2
-    elif month == 'april':
+    elif month == 4:
         number = 3
-    elif month == 'may':
+    elif month == 5:
         number = 4
-    elif month == 'june':
+    elif month == 6:
         number = 5
-    elif month == 'july':
+    elif month == 7:
         number = 6
-    elif month == 'august':
+    elif month == 8:
         number = 7
-    elif month == 'september':
+    elif month == 9:
         number = 8
-    elif month == 'october':
+    elif month == 10:
         number = 9
-    elif month == 'november':
+    elif month == 11:
         number == 10
     else:
         number == 11
@@ -142,14 +145,13 @@ def get_climate(latitude, longitude, month):
     high = (temp_max * 1.8) + 32
     low = (temp_min * 1.8) + 32
     rain = (total_rain * 2) / 25.4
-    print(f'The average high is {high:.2f}F')
-    print(f'The average low is {low:.2f}F')
-    print(f'The average rainfall is {rain:.2f} inches per month')
-    print(f'The average hours of sunshine per day is {sunshine_hours:.2f}')
+    rain_return =f' {high:.2f}F'
+    low_return = f' {low:.2f}F'
+    rain_return = f'{rain:.2f} inches per month'
+    high_return = f'{sunshine_hours:.2f}'
 
 
 
 
 
-if __name__ == '__main__':
-    main()
+
