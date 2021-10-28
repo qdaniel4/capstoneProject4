643470fe6ba6e2ec1dd2cf65df9af5c601115cf0
saida93522 <54591143+saida93522@users.noreply.github.com>
2021-10-27 20:31:39 -0500

    fix credentials

--- holiday_cal/credentials.py
+++ holiday_cal/credentials.py
@@ -1,7 +1,7 @@
 """ apis and urls"""
 import os
-# api_key = ['YOUR API KEY HERE']
-api_key = '6d9647f22823956bb70345aa970d74ae6e725917'
+api_key = ['YOUR API KEY HERE']
+
 
 # url
 url_countries = 'https://calendarific.com/api/v2/countries'
